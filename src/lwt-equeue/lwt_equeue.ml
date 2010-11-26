596ce33642f7db1bf36c120f1a9b9f8ee4c0f63c
Jake Donham <jake@donham.org>
2010-11-26 16:34:32 -0500

    track and diff fd sets so we don't add/remove them all on every event

--- src/lwt-equeue/lwt_equeue.ml
+++ src/lwt-equeue/lwt_equeue.ml
@@ -1,6 +1,9 @@
 let event_system = ref None
 let group = ref None
-let old_resources = ref []
+let old_set_r = ref []
+let old_set_w = ref []
+let old_set_e = ref []
+let old_timeout = ref None
 let in_start = ref false
 
 let get_event_system () =
@@ -13,36 +16,51 @@ let get_group () =
     | None -> failwith "event system not set"
     | Some g -> g
 
-let select set_r set_w set_e timeout =
-  begin match set_r, set_w, set_e, timeout with
-    | [], [], [], Some 0.0 -> ()
-    | _ ->
-        let es = get_event_system () in
-        let g = get_group () in
-        let rs = List.fold_left (fun rs fd -> (Unixqueue.Wait_in fd, -1.)::rs) [] set_r in
-        let rs = List.fold_left (fun rs fd -> (Unixqueue.Wait_out fd, -1.)::rs) rs set_w in
-        let rs = List.fold_left (fun rs fd -> (Unixqueue.Wait_oob fd, -1.)::rs) rs set_e in
-        let rs =
-          match timeout with
-            | None -> rs
-            | Some timeout -> (Unixqueue.Wait (es#new_wait_id ()), timeout)::rs in
-        List.iter (fun r -> es#add_resource g r) rs;
-        old_resources := rs
+(* fd sets from Lwt are sorted in decreasing order *)
+let add_remove_resources add es g con old_set set =
+  let print_fd_list s set =
+    Printf.eprintf "%s: [ %s ]\n" s (String.concat ", " (List.map (fun fd -> string_of_int (Obj.magic fd)) set)) in
+  print_fd_list "old_set" old_set;
+  print_fd_list "set" set;
+  let rec loop old_set set =
+    match old_set, set with
+      | [], [] -> ()
+      | [], _ -> if add then List.iter (fun fd -> es#add_resource g (con fd, -1.)) set
+      | _, [] -> List.iter (fun fd -> es#remove_resource g (con fd)) old_set
+      | oh :: old_set', h :: set' ->
+          if oh = h then loop old_set' set'
+          else if oh > h then (es#remove_resource g (con oh); loop old_set' set)
+          else (if add then es#add_resource g (con h, -1.); loop old_set set') in
+  loop old_set set
+
+let select add set_r set_w set_e timeout =
+  let es = get_event_system () in
+  let g = get_group () in
+  add_remove_resources add es g (fun fd -> Unixqueue.Wait_in fd) !old_set_r set_r;
+  add_remove_resources add es g (fun fd -> Unixqueue.Wait_out fd) !old_set_w set_w;
+  add_remove_resources add es g (fun fd -> Unixqueue.Wait_oob fd) !old_set_e set_e;
+  old_set_r := set_r;
+  old_set_w := set_w;
+  old_set_e := set_e;
+  begin match !old_timeout with
+    | None -> ()
+    | Some t -> es#remove_resource g t
+  end;
+  begin match timeout with
+    | None | Some 0.0 -> old_timeout := None
+    | Some ts ->
+        let r = Unixqueue.Wait (es#new_wait_id ()) in
+        if add then es#add_resource g (r, ts);
+        old_timeout := Some r
   end;
   (lazy 0., [], [], [])
 
-let add_resources () =
+let update_resources add =
   Lwt.wakeup_paused ();
-  ignore (Lwt_main.apply_filters select [] [] [] None)
-
-let remove_resources () =
-  let es = get_event_system () in
-  let g = get_group () in
-  List.iter (fun (r, _) -> es#remove_resource g r) !old_resources;
-  old_resources := []
+  ignore (Lwt_main.apply_filters (select add) [] [] [] None)
 
 let iteration () =
-  add_resources ();
+  update_resources true;
   let es = get_event_system () in
   es#run ()
 
@@ -58,8 +76,7 @@ let handler _ _ e =
     (Lwt_main.apply_filters
        (fun _ _ _ _ -> Lazy.lazy_from_fun Unix.gettimeofday, set_r, set_w, set_e)
        [] [] [] None);
-  remove_resources ();
-  if !in_start then add_resources ()
+  update_resources !in_start
 
 let _ = Lwt_main.main_loop_iteration := iteration
 
@@ -76,9 +93,12 @@ let unset_event_system () =
   es#clear g;
   event_system := None;
   group := None;
-  old_resources := []
+  old_set_r := [];
+  old_set_w := [];
+  old_set_e := [];
+  old_timeout := None
 
 let start t =
   in_start := true;
   Lwt.ignore_result (t ());
-  add_resources ()
+  update_resources true
